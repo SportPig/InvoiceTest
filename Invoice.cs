755d840e91434ec145a3716069f7f888ac50f400
Bryan Williams <bryan.williams@hrblock.com>
2022-06-10 14:24:56 -0600

    Add project files.

---
+++ Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceTest
+{
+    class Invoice
+    {
+        string partNumber;
+        string partDescription;
+        int quantity;
+        double price;
+
+        public string PartNumber
+        {
+            get { return partNumber; }
+            set { partNumber = value; }
+        }
+
+        public string PartDescription
+        {
+            get { return partDescription; }
+            set { partDescription = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0) value = 0;
+                price = value;
+            }
+        }
+
+        public Invoice(string partNumber, string partDescription, int quantity, double price)
+        {
+            PartNumber = partNumber;
+            PartDescription = partDescription;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double getInvoiceAmount()
+        {
+            return quantity * price;
+        }
+
+    }
+}
+
+
---
+++ InvoiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace InvoiceTest
+{
+    class InvoiceTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Harry's Hardware Customer Invoice" + "!");
+            Console.WriteLine(("").PadRight(34, '-'));
+            Console.WriteLine();
+
+            //create object of Invoice
+            Invoice inv = new Invoice("Z58966TG776", "Brake Pads", 25, 52.69);
+            double cost = inv.getInvoiceAmount();
+            Console.WriteLine("Part Number = {0}", inv.PartNumber, inv.PartDescription, inv.Quantity, inv.Price, cost);
+            Console.WriteLine();
+            Console.WriteLine("Part Description = {1}", inv.PartNumber, inv.PartDescription, inv.Quantity, inv.Price, cost);
+            Console.WriteLine();
+            Console.WriteLine("Part Quantity = {2}", inv.PartNumber, inv.PartDescription, inv.Quantity, inv.Price, cost);
+            Console.WriteLine();
+            Console.WriteLine("Price per Item = $ {3}", inv.PartNumber, inv.PartDescription, inv.Quantity, inv.Price, cost);
+            Console.WriteLine();
+            Console.WriteLine(("").PadRight(34, '-'));
+            Console.WriteLine();
+            Console.WriteLine("Total Cost = $ {4}", inv.PartNumber, inv.PartDescription, inv.Quantity, inv.Price, cost);
+            Console.WriteLine();
+        }
+    }
+}
